98d51e936176cda4c709b159313d9a6ee8905b01
Bisrat Shiferaw <57441461+BisratS@users.noreply.github.com>
2022-12-23 09:18:41 +0300

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa"""
-import MySQLdb
-import sys
+"""Script lists all states with a name starting with N from database
+Takes three arguments:
+    mysql username
+    mysql password
+    database name
+Connects to default host (localhost) and port (3306)
+"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
-    """In order to put our new connnection to good use we
-     need to create a cursor object"""
-    cur = db.cursor()
-    """The execute function requires one parameter, the query."""
-    cur.execute("SELECT * FROM states\
-        WHERE name REGEXP BINARY '^N'\
-            ORDER BY id ASC")
-    """Obtaining Query Results"""
-    rows = cur.fetchall()
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name like "N%"\
+            ORDER BY states.id ASC""")
+    rows = c.fetchall()
     for row in rows:
-        print(row)
-    """ Close all cursors"""
-    cur.close()
-    """Close all databases"""
+        if row[1][0] == 'N':
+            print(row)
+    c.close()
     db.close()
