fe4810939d2cd8bb8453fe4d5bd9d716fb5a6f7e
Bisrat Shiferaw <57441461+BisratS@users.noreply.github.com>
2023-02-23 11:59:39 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """ Finds a peak inside a list """
 
-#!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers
-"""
-
-
 def find_peak(list_of_integers):
     """
     Args:
